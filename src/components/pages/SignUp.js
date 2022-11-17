78ee9e4f0926d3934ffc0ede26d1d280cbb671f1
Carlos Hernandez <carloshdzrco@gmail.com>
2022-11-17 17:54:57 -0600

    added navbar to sign up page

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,5 +1,6 @@
 // SignUp template provided by Material UI
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useNavigate } from 'react-router-dom'
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
@@ -11,7 +12,7 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
+import PublicNavbar from '../layout/navbars/PublicNavbar';
 
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
@@ -25,7 +26,17 @@ const theme = createTheme();
 export default function SignUp() {
 
   const [city, setCity] = useState('');
-  const [host, setHost] = useState(false) 
+  const [host, setHost] = useState(false)
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+
+    if(localStorage.getItem('eventsHubInfo') != null) {
+      navigate('/searchevents')
+    }
+
+  }, [])
 
   const handleChangeCity = (event) => {
     setCity(event.target.value);
@@ -77,111 +88,115 @@ export default function SignUp() {
     });
   };
 
-  return (
-    <>
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Typography component="h1" variant="h5">
-            Sign Up
-          </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                  <FormControl sx={{ m: 0, minWidth: 400 }}>
-                    <InputLabel id="demo-simple-select-helper-label">City</InputLabel>
-                    <Select
-                      labelId="demo-simple-select-helper-label"
-                      id="demo-simple-select-helper"
-                      value={city}
-                      label="City"
-                      onChange={handleChangeCity}
-                    >
-                      <MenuItem value={'Atlanta'}>Atlanta </MenuItem>
-                      <MenuItem value={'Nashville'}>Nashville</MenuItem>
-                      <MenuItem value={'Richmond'}>Richmond</MenuItem>
-                    </Select>
-                  </FormControl>
-              </Grid>
-              <Grid item xs={12}>
-                {/* Modify the id, name, and autoComplete to relate to our project */}
-                <TextField
-                  required
-                  fullWidth
-                  id="Username"
-                  label="Username"
-                  name="Username"
-                  autoComplete="Username"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  // Modify the value to relate to our project
-                  control={<Checkbox  value="host" onChange={(e)=>handleChange(e)}  color="primary" />}
-                  label="I want to be a host and create events."
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="#" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
+  if(localStorage.getItem('eventsHubInfo') === null ) {
+    return (
+      <>
+      <PublicNavbar />
 
-    </>
-  );
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              Sign Up
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    autoComplete="given-name"
+                    name="firstName"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="First Name"
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="lastName"
+                    label="Last Name"
+                    name="lastName"
+                    autoComplete="family-name"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                    <FormControl sx={{ m: 0, minWidth: 400 }}>
+                      <InputLabel id="demo-simple-select-helper-label">City</InputLabel>
+                      <Select
+                        labelId="demo-simple-select-helper-label"
+                        id="demo-simple-select-helper"
+                        value={city}
+                        label="City"
+                        onChange={handleChangeCity}
+                      >
+                        <MenuItem value={'Atlanta'}>Atlanta </MenuItem>
+                        <MenuItem value={'Nashville'}>Nashville</MenuItem>
+                        <MenuItem value={'Richmond'}>Richmond</MenuItem>
+                      </Select>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12}>
+                  {/* Modify the id, name, and autoComplete to relate to our project */}
+                  <TextField
+                    required
+                    fullWidth
+                    id="Username"
+                    label="Username"
+                    name="Username"
+                    autoComplete="Username"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="new-password"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControlLabel
+                    // Modify the value to relate to our project
+                    control={<Checkbox  value="host" onChange={(e)=>handleChange(e)}  color="primary" />}
+                    label="I want to be a host and create events."
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign Up
+              </Button>
+              <Grid container justifyContent="flex-end">
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    Already have an account? Sign in
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Container>
+      </ThemeProvider>
+
+      </>
+    );
+  }
 }
